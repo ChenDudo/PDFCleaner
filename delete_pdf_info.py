c9d5b08c9ea5a57c0247fe4ad038d19127a21529
chendo <chendu@mindmotion.com.cn>
2022-12-05 17:58:08 +0800

    upgarde scripts

--- delete_pdf_info.py
+++ delete_pdf_info.py
@@ -4,27 +4,39 @@
 
 from  PyPDF2 import PdfFileReader
 from  PyPDF2 import PdfWriter
+import os
+import time
 
+global handle_num
+output_file = []
 
-def handle_doc(filepath):
+handle_num = 0;
 
-    # with open(filepath, 'rb') as f:
-    reader = PdfFileReader(filepath)
-    infor = reader.getDocumentInfo()
-    print(infor)
-    print("=========")
-    meta = reader.metadata
-    l_creation_data = str(meta.creation_date)
-    l_creation_data_raw = str(meta.creation_date_raw)
-    l_modification_date = str(meta.modification_date)
-    l_modification_date_raw = str(meta.modification_date_raw)
-    # print("author" + meta.author)
-    # print("creator:" + meta.creator)
-    print("producer" + meta.producer)
-    print(meta.subject)
-    print(meta.title)
+def handle_doc(outputfilepath, filepath, filename):
+    NanjingProducer = "MindMotion Nanjing Ecosystem"
+    
+    global handle_num
+    readfilepath = filepath + '\\' + filename
+    outputfilepath = outputfilepath + '\\' + filename
 
+    if not filename.endswith('.pdf'):
+        print("[Error]\t\""+filename+'\" is not *.Pdf files!')
+        return
+
+    # reader & writer
+    reader = PdfFileReader(readfilepath)
     writer = PdfWriter()
+
+    # read meta
+    meta = reader.metadata
+
+    l_creation_data_raw = str(meta.creation_date_raw)
+    l_modification_date_raw = str(meta.modification_date_raw)
+
+    if (meta.producer == NanjingProducer):
+        print("[Warning]\t\""+filename+'\" needn\'t to change!')
+        return
+
     # Add all pages to the writer
     for page in reader.pages:
         writer.add_page(page)
@@ -34,17 +46,42 @@ def handle_doc(filepath):
         {
             "/Author": "Firmware Development Group",
             "/Creator": "Chen Do",
-            "/Producer": "MindMotion Nanjing Ecosystem",
+            "/Producer": NanjingProducer,
             "/CreationDate": l_creation_data_raw,
             "/ModDate": l_modification_date_raw
         }
     )
 
+    handle_num = handle_num + 1
+    output_file.append(outputfilepath)
+
     # Save the new PDF to a file
-    with open("./1_copy.pdf", "wb") as f:
+    with open(outputfilepath, "wb") as f:
         writer.write(f)
+        print('[info]'+'*'*12+'\t\"'+filename+"\" Done!\t"+'*'*12)
 
 if __name__ == "__main__":
-    print("-------begin-------")
-    filepath = "./doc/1.pdf"
-    handle_doc(filepath)
+
+    print("-------Begin-------")
+    time_start = time.time()  # 记录开始时间
+    
+    # get current file path
+    # path_files = os.path.dirname(os.path.abspath(__file__))+'\\doc'
+    path_files = os.path.dirname(os.path.abspath(__file__))
+
+    # output file path
+    output_filepath = os.path.dirname(os.path.abspath(__file__))+'\\output'
+    if not os.path.exists(output_filepath):
+        print("[info]\tNow is making output files dir!")
+        os.makedirs(output_filepath)
+    print('[NOTE]\tOutput Dir: ' + output_filepath)
+
+    for filepath, dirnames, filenames in os.walk(path_files):
+        for filename in filenames:
+            handle_doc(output_filepath, filepath, filename)
+
+    time_end = time.time()
+    time_sum = time_end - time_start
+    print("[info]\tTotal Speed Time: %.2f s, Handle: %d files" %(time_sum,handle_num))
+    for printList in output_file:
+        print('\t'+printList)
